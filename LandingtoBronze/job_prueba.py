ce562fada78de593632ff4215f5954000827e015
Paloma Carmona <enfoque711@cpsaa.com.pe>
2025-04-25 17:07:14 -0500

    Otro  intento

--- LandingtoBronze/job_prueba.py
+++ LandingtoBronze/job_prueba.py
@@ -8,9 +8,16 @@ from awsglue.job import Job
 ## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
+
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
+
+dyf_dummy = glueContext.create_dynamic_frame.from_catalog(
+    database="db_landing",
+    table_name="ld_teams"
+)
+
 job.commit()
