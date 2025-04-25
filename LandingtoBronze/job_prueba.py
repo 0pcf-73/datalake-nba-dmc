6d86c311dc1f2fb294cc4230b3fdc39a745eb1d1
Paloma Carmona <enfoque711@cpsaa.com.pe>
2025-04-25 17:15:39 -0500

    nuevo codigo

--- LandingtoBronze/job_prueba.py
+++ LandingtoBronze/job_prueba.py
@@ -1,4 +1,5 @@
 ## @type: job
+
 import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
@@ -6,16 +7,14 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-
-
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
+# Leer una tabla real (aunque no la uses)
 dyf_dummy = glueContext.create_dynamic_frame.from_catalog(
     database="db_landing",
     table_name="ld_teams"
