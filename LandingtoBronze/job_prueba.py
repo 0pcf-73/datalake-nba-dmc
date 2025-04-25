c37cc080448f491116c7af1b0ad3bd9d81911516
0pcf-73 <ypaloma.carmonaf@gmail.com>
2025-04-25 17:18:33 -0500

    Synchronizing job job_prueba from AWS Glue at 2025/04/25 22:18:33

--- LandingtoBronze/job_prueba.py
+++ LandingtoBronze/job_prueba.py
@@ -1,5 +1,3 @@
-## @type: job
-
 import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
@@ -7,17 +5,12 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
+## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
-
-# Leer una tabla real (aunque no la uses)
-dyf_dummy = glueContext.create_dynamic_frame.from_catalog(
-    database="db_landing",
-    table_name="ld_teams"
-)
-
 job.commit()
